72495753bfec1bf1e22a6980a9b5df156da4d158
EmbeddedMhawar <abderrahmanemhawar@gmail.com>
2024-07-05 07:20:26 +0100

    end of basis

--- C-Basics.c
+++ C-Basics.c
@@ -120,3 +120,131 @@ int main(){
 
 }########################################################################
 */
+/*########################################################################
+
+
+EX3:
+
+Write C Program to Add Two Integers
+
+i should see the Console as following:
+##########Console-output###
+Enter two integers: 12
+11
+Sum: 23
+###########################
+
+#include "stdio.h"
+
+int main(){
+int i,j;
+    printf("Enter two integers:");
+    scanf("%d",&i);
+    scanf("%d",&j);
+    printf("Sum: %d", i+j);
+
+}
+########################################################################
+
+########################################################################
+
+
+EX4:
+
+Write C Program to Multiply two Floating Point Numbers
+i should see the Console as following:
+##########Console-output###
+
+###########################
+Enter two numbers: 2.4
+1.1
+Product: 2.640000
+########################################################################
+
+
+
+#include "stdio.h"
+
+int main(){
+float i,j;
+printf("Enter two numbers: ");
+    scanf("%f",&i);
+    scanf("%f",&j);
+    printf("Product: %f", i*j);
+}
+
+########################################################################
+
+
+
+EX5:
+
+Write C Program to Find ASCII Value of a Character
+
+#########Console_output######
+Enter a character: G
+ASCII value of G = 71
+#############################
+
+
+########################################################################
+#include "stdio.h"
+
+int main(){
+char i;
+printf("Enter a character: ");
+    scanf("%s",&i);
+    printf("ASCII value of G = %d", i);
+}
+########################################################################
+
+EX6:
+
+Write Source Code to Swap Two Numbers
+
+#########Console_output######
+Enter value of a: 1.20
+Enter value of b: 2.45
+
+After swapping, value of a = 2.45
+After swapping, value of b = 1.2
+-----------------------------------
+
+#include "stdio.h"
+
+int main(){
+float i,j,k;
+    printf("Enter value of a: ");
+    scanf("%f",&i);
+    printf("Enter value of b: ");
+    scanf("%f",&j);
+    k=i;
+    i=j;
+    j=k;
+    printf("After swapping, value of a = %.2f \n", i);
+    printf("After swapping, value of b = %.1f", j);
+
+}
+########################################################################
+
+EX7:
+it is an interview trick
+
+Write Source Code to Swap Two Numbers without temp variable.
+
+#include "stdio.h"
+
+int main(){
+float i,j;
+    printf("Enter value of a: ");
+    scanf("%f",&i);
+    printf("Enter value of b: ");
+    scanf("%f",&j);
+    i=i+j;
+    j=i-j;
+    i=i-j;
+    printf("2After swapping, value of a = %.2f \n", i);
+    printf("2After swapping, value of b = %.1f", j);
+
+}
+*/
